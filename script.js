41ac929e1f99294967cf0b5591b5f1e4fadb0d8b
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-04-07 02:29:06 +0600

    variable and constant

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('This is first script');
+
+//it is ok to not add let js autometically add it
+let test_num = 1;
+alert(test_num)
+test_num =2;
+alert(test_num)
+
+const UNI = "IUT";
+alert(UNI)
