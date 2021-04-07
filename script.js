e8afecadfcabe3eb09280d444f55ba2460434fff
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-04-07 20:17:14 +0600

    if else condition and for-while loop

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-let result = confirm("Are you 25 years old?");
-alert(result)
+let a = b =c = 5;
+
+if (a!=b){
+    alert('Inside if')
+}
+else if (b!=c){
+    alert('Inside else if')
+}
+else{
+    alert('Inside else')
+}
+
+//for loop
+let str = '';
+for(let i=0;i<5;i++){
+
+    str+=i;
+    str+='\n'
+}
+
+alert(str)
+
+let i =0;
+let str2 = ''
+while(i<10){
+    str2+=i;
+    str2+='\n';
+    i++;
+}
+alert(str2);
