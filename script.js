a1ce50a88ef40eee1191af043ee15369b5743c50
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-04-07 20:05:41 +0600

    operators and prompt

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-
-//it is ok to not add let js autometically add it
-let test_num = 1;
-alert(test_num)
-test_num =2;
-alert(test_num)
-
-const UNI = "IUT";
-alert(UNI)
+let result = prompt("How old are you?", "10");
+alert(result)
